2562f41cfdba6e9fb17da747f81ec405a1409c0a
hyoungjook <hyoungjoo.cs@gmail.com>
2026-03-19 16:30:42 -0400

    slab allocator subwarp support

--- include/simple_slab_alloc.hpp
+++ include/simple_slab_alloc.hpp
@@ -28,15 +28,14 @@ template <uint32_t slab_size = 128, std::size_t max_bytes = 6UL * 1024 * 1024 * 
 struct simple_slab_allocator {
   using size_type = uint32_t;
   using pointer_type = size_type;
-  using bitmap_type = uint32_t;
   static constexpr uint32_t slab_size_ = slab_size;
-  static constexpr uint32_t tile_size_ = 32;
-  static constexpr uint32_t num_slabs_in_block_ = tile_size_ * sizeof(bitmap_type) * 8;
+  static constexpr uint32_t num_slabs_in_block_ = 32 * sizeof(uint32_t) * 8;
+  static_assert(num_slabs_in_block_ == 16 * sizeof(uint64_t) * 8);
   static constexpr uint32_t block_size_ = slab_size_ * num_slabs_in_block_;
   static constexpr std::size_t total_blocks_ = max_bytes / block_size_;
   static constexpr std::size_t total_bytes_ = total_blocks_ * block_size_;
   static constexpr std::size_t total_bitmap_bytes_ = (total_blocks_ * num_slabs_in_block_) / 8;
-  static_assert(total_bitmap_bytes_ % (sizeof(bitmap_type) * tile_size_) == 0);
+  static_assert(total_bitmap_bytes_ % (num_slabs_in_block_ / 8) == 0);
   struct device_instance_type {
     void* pool_;
   };
@@ -56,6 +55,7 @@ struct simple_slab_allocator {
   device_instance_type get_device_instance() const { return device_instance_type{pool_}; }
 
   void print_stats() const {
+    using bitmap_type = uint32_t;
     bitmap_type *h_bitmap = new bitmap_type[total_bitmap_bytes_ / sizeof(bitmap_type)];
     cuda_try(cudaMemcpy(h_bitmap,
                         reinterpret_cast<void*>(reinterpret_cast<uint8_t*>(pool_) - total_bitmap_bytes_),
@@ -82,13 +82,12 @@ struct device_allocator_context<simple_slab_allocator<slab_size, max_bytes>> {
   using device_instance_type = typename host_alloc_type::device_instance_type;
   using size_type = typename host_alloc_type::size_type;
   using pointer_type = typename host_alloc_type::pointer_type;
-  using bitmap_type = typename host_alloc_type::bitmap_type;
 
   template <typename tile_type>
   DEVICE_QUALIFIER device_allocator_context(const device_instance_type& alloc, const tile_type& tile)
       : alloc_(alloc) {
-    assert(tile.size() == tile_size_);
-    block_index_ = initialize_index(get_tile_id());
+    static_assert(tile_type::size() == 32 || tile_type::size() == 16);
+    block_index_ = initialize_index(get_tile_id<tile_type>());
   }
 
   template <typename tile_type>
@@ -124,7 +123,6 @@ struct device_allocator_context<simple_slab_allocator<slab_size, max_bytes>> {
 
 private:
   static constexpr uint32_t slab_size_ = host_alloc_type::slab_size_;
-  static constexpr uint32_t tile_size_ = host_alloc_type::tile_size_;
   static_assert(host_alloc_type::total_blocks_ * host_alloc_type::num_slabs_in_block_ < std::numeric_limits<pointer_type>::max());
   static constexpr uint32_t num_slabs_in_block_ = host_alloc_type::num_slabs_in_block_;
   static constexpr uint32_t total_blocks_ = static_cast<uint32_t>(host_alloc_type::total_blocks_);
@@ -137,8 +135,9 @@ private:
   pointer_type block_index_;
 
   static constexpr uint32_t mix_prime = 0x9e3779b1;
+  template <typename tile_type>
   DEVICE_QUALIFIER uint32_t get_tile_id() const {
-    return (threadIdx.x + blockIdx.x * blockDim.x) / tile_size_;
+    return (threadIdx.x + blockIdx.x * blockDim.x) / tile_type::size();
   }
   DEVICE_QUALIFIER pointer_type initialize_index(uint32_t tile_id) const {
     return static_cast<pointer_type>(tile_id * mix_prime) % total_blocks_;
@@ -152,6 +151,7 @@ private:
 
   template <typename tile_type>
   DEVICE_QUALIFIER pointer_type try_allocate_in_block(pointer_type block_index, const tile_type& tile) {
+    using bitmap_type = std::conditional_t<tile_type::size() == 32, uint32_t, uint64_t>;
     auto* bitmap_base = reinterpret_cast<uint8_t*>(alloc_.pool_) - total_bitmap_bytes_;
     bitmap_type* bitmap_addr =
         reinterpret_cast<bitmap_type*>(
@@ -159,7 +159,13 @@ private:
     bitmap_type bitmap = *bitmap_addr;
     pointer_type result = invalid_pointer;
     while (result == invalid_pointer) {
-      auto empty_lane = __ffs(~bitmap) - 1;
+      int empty_lane;
+      if constexpr (tile_type::size() == 32) {
+        empty_lane = __ffs(~bitmap) - 1;
+      }
+      else {
+        empty_lane = __ffsll(~bitmap) - 1;
+      }
       auto free_lane = tile.ballot(empty_lane >= 0);
       if (free_lane != 0) {
         uint32_t src_lane = __ffs(free_lane) - 1;
@@ -183,6 +189,7 @@ private:
     return result;
   }
   DEVICE_QUALIFIER void deallocate_in_block(pointer_type ptr) {
+    using bitmap_type = uint32_t;
     auto* bitmap_base = reinterpret_cast<uint8_t*>(alloc_.pool_) - total_bitmap_bytes_;
     auto bitmap_index = ptr / (sizeof(bitmap_type) * 8);
     auto bit_index = ptr % (sizeof(bitmap_type) * 8);
